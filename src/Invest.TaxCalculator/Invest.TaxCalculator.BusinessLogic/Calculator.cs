3a8c3656959a33d0cd1b7728e87a35fb1dae1bd7
Roman Puzanov <puzanov.rs@skbkontur.ru>
2023-01-11 18:32:03 +0500

    Заготовка калькулятора

--- src/Invest.TaxCalculator/Invest.TaxCalculator.BusinessLogic/Calculator.cs
+++ src/Invest.TaxCalculator/Invest.TaxCalculator.BusinessLogic/Calculator.cs
@@ -1,6 +1,46 @@
-﻿namespace Invest.TaxCalculator.BusinessLogic
+﻿using System;
+
+namespace Invest.TaxCalculator.BusinessLogic
 {
-    public class Calculator
+    /// <summary>
+    ///     Калькулятор транзакций
+    /// </summary>
+    public class TransactionsCalculator
+    {
+        public TransactionBase[] Calculate(Operation[] operations, TransactionBase[] oldTransactions, int year)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    ///     Покупка/Продажа/Дивиденды/Комиссия брокера
+    /// </summary>
+    public class Operation
+    {
+        
+    }
+
+    /// <summary>
+    ///     Базовый класс для сделок
+    /// </summary>
+    public abstract class TransactionBase
+    {
+        
+    }
+
+    /// <summary>
+    ///     Сделка по купле продаже
+    /// </summary>
+    public class BuySellTransaction
+    {
+        
+    }
+
+    /// <summary>
+    ///     Дивиденды
+    /// </summary>
+    public class DividendsTransaction
     {
         
     }
