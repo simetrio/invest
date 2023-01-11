b224665ce1bc319da37cf57fb213992df346215f
Roman Puzanov <puzanov.rs@skbkontur.ru>
2023-01-12 01:32:07 +0500

    Рассчитываю профит

--- src/Invest.TaxCalculator/Invest.TaxCalculator.BusinessLogic/Transactions/Transaction.cs
+++ src/Invest.TaxCalculator/Invest.TaxCalculator.BusinessLogic/Transactions/Transaction.cs
@@ -1,12 +1,16 @@
+using System.Linq;
+
 namespace Invest.TaxCalculator.BusinessLogic.Transactions
 {
     public class Transaction
     {
         public string Ticker { get; set; }
-        
+
         public TransactionType Type { get; set; }
-        
+
         public TransactionOperation[] Operations { get; set; }
+
+        public decimal Profit { get; set; }
 
         public static Transaction Create(string ticker, TransactionType type, TransactionOperation[] operations)
         {
@@ -15,7 +19,20 @@ namespace Invest.TaxCalculator.BusinessLogic.Transactions
                 Ticker = ticker,
                 Type = type,
                 Operations = operations,
+                Profit = CalculateProfit(operations),
             };
+        }
+
+        private static decimal CalculateProfit(TransactionOperation[] operations)
+        {
+            return Calculate(TransactionOperationType.Credit) - Calculate(TransactionOperationType.Debit);
+
+            decimal Calculate(TransactionOperationType type)
+            {
+                return operations
+                    .Where(x => x.Type == type)
+                    .Sum(x => x.Price * x.Count * x.DollarPrice);
+            }
         }
     }
 }
