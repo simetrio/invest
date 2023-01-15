807e5f690cf8e5609679ae3b612d8dd45e685e73
Roman Puzanov <puzanov.rs@skbkontur.ru>
2023-01-15 10:09:04 +0500

    Конфигурация контейнера

---
+++ src/Invest.TaxCalculator/Invest.TaxCalculator/BusinessLogic/Container.cs
@@ -0,0 +1,10 @@
+﻿namespace Invest.TaxCalculator.BusinessLogic
+{
+    public static class Container
+    {
+        public static void ConfigureContainer(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddSingleton<WeatherForecastService>();
+        }
+    }
+}
--- src/Invest.TaxCalculator/Invest.TaxCalculator/Program.cs
+++ src/Invest.TaxCalculator/Invest.TaxCalculator/Program.cs
@@ -1,13 +1,11 @@
 using Invest.TaxCalculator.BusinessLogic;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+builder.ConfigureContainer();
 
 var app = builder.Build();
 
