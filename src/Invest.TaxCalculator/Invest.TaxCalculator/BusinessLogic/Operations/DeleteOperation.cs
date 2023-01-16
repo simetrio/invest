b223c616f3fb031efa5a8b8332887fa9ff2a6f5b
Roman Puzanov <puzanov.rs@skbkontur.ru>
2023-01-16 10:15:19 +0500

    Удаление операции

---
+++ src/Invest.TaxCalculator/Invest.TaxCalculator/BusinessLogic/Operations/DeleteOperation.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+
+namespace Invest.TaxCalculator.BusinessLogic.Operations
+{
+    public class DeleteOperationHandler : IRequestHandler<DeleteOperationCommand>
+    {
+        private readonly Mapper _mapper = new();
+        private readonly OperationsService _operationsService = new();
+
+        public Task<Unit> Handle(DeleteOperationCommand request, CancellationToken cancellationToken)
+        {
+            var operation = _operationsService.ReadAll().Single(x => x.Id == request.Id);
+            _operationsService.Delete(operation);
+
+            return Unit.Task;
+        }
+    }
+
+    public class DeleteOperationCommand : IRequest
+    {
+        public DeleteOperationCommand(string id)
+        {
+            Id = id;
+        }
+
+        public string Id { get; }
+    }
+}
