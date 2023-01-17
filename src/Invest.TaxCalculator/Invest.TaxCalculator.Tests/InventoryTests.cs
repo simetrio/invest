4d6e7537d4264b4deb077fac202af9f0969a7310
Roman Puzanov <puzanov.rs@skbkontur.ru>
2023-01-17 22:38:00 +0500

    Тест на инвентаризацию

---
+++ src/Invest.TaxCalculator/Invest.TaxCalculator.Tests/InventoryTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using FluentAssertions;
+using Invest.TaxCalculator.BusinessLogic.Inventories;
+using Invest.TaxCalculator.Tests.Utils;
+using NUnit.Framework;
+
+namespace Invest.TaxCalculator.Tests
+{
+    public class InventoryTests
+    {
+        private readonly Inventory _inventory = new();
+
+        [Test]
+        public void Calculate()
+        {
+            var builder = new EntityBuilder()
+                    .WithBuySellShare("AC1", DateTime.Now, 9, 150, 71, DateTime.Now, 7, 150, 71, 0.01m)
+                    .WithBuySellShare("AC1", DateTime.Now, 5, 150, 71, DateTime.Now, 6, 150, 71, 0.01m)
+                    .WithBuyShare("AC1", DateTime.Now, 4, 150, 71, 0.01m)
+                    .WithBuyBond("AC2", DateTime.Now, 8, 150, 71, 0.01m);
+
+            var expected = new[]
+            {
+                new InventoryItem
+                {
+                    Ticker = "AC1",
+                    Count = 5,
+                },
+                new InventoryItem
+                {
+                    Ticker = "AC2",
+                    Count = 8,
+                },
+            };
+
+            var actual = _inventory.Calculate(builder.Operations);
+
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}
